05dcd705c7887db40168a0d526a23411a31d8229
George Dwyer <george.dwyer.14@ucl.ac.uk>
2017-10-23 00:10:30 +0100

    Update joint_publisher_node.cpp

--- joint_control/src/joint_publisher_node.cpp
+++ joint_control/src/joint_publisher_node.cpp
@@ -4,7 +4,7 @@ int main(int argc, char **argv){
 	ros::init(argc, argv, "joint_publisher");
 	ros::NodeHandle nh;
 	
-	// TODO: Write subscribers
+	// TODO: Write publisher
 	ROS_INFO("\nWrite your publisher here!\n");
 	return 0;	
 }
