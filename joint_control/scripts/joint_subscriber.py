1e6e24d5c8d7a09a48a4d8bfa06996d73ff23009
George Dwyer <george.dwyer.14@ucl.ac.uk>
2017-10-22 15:24:55 +0100

    Added files to start writting the subscriber in python and cpp

---
+++ joint_control/scripts/joint_subscriber.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+import rospy
+
+if __name__ == '__main__':
+	rospy.init_node('joint_subscriber')
+	rospy.loginfo('\nMake your subscriber here!\n')
+
---
+++ joint_control/src/joint_subscriber_node.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv){
+	ros::init(argc, argv, "joint_subscriber");
+	ros::NodeHandle nh;
+	
+	// TODO: Write subscribers
+	ROS_INFO("\nWrite your subscriber here!\n");
+	return 0;	
+}
