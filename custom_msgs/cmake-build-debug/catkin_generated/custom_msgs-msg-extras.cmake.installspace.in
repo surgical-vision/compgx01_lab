e2a90afe1a2e22faba73c7a0cfbe808ebd424034
K Pachtrachai <krittin.pachtrachai.13@ucl.ac.uk>
2017-10-30 15:50:24 +0000

    Delete rubbish files

--- custom_msgs/cmake-build-debug/catkin_generated/custom_msgs-msg-extras.cmake.installspace.in
+++
@@ -1,2 +0,0 @@
-set(custom_msgs_MESSAGE_FILES "msg/EulerMsg.msg;msg/AngleAxisMsg.msg;msg/RotMatMsg.msg")
-set(custom_msgs_SERVICE_FILES "")
--- custom_msgs/cmake-build-debug/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/kpach/catkin_ws/src/compgx01_lab/custom_msgs/cmake-build-debug/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
