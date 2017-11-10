9859d5f1dde08597bba57b6397ca2b9e28905bb0
K Pachtrachai <krittin.pachtrachai.13@ucl.ac.uk>
2017-11-10 23:57:26 +0000

    add cpp scripts

--- inverse_kinematics/include/inverse_kinematics/YoubotKDL.h
+++ inverse_kinematics/include/inverse_kinematics/YoubotKDL.h
@@ -5,22 +5,18 @@
 
 class YoubotKDL : public YoubotKinematics
 {
-private:
-
+public:
     KDL::Frame current_pose;
     KDL::Chain kine_chain;
     KDL::JntArray current_joint_position;
 
-
-public:
-
     int init();
-    void broadcast_pose();
+    void broadcast_pose(KDL::Frame current_pose);
     void setup_kdl_chain();
-    KDL::Jacobian get_jacobian(KDL::ChainJntToJacSolver jac_solver);
-    KDL::JntArray inverse_kinematics_closed(KDL::JntArray current_joint_position, KDL::Frame desired_pose, KDL::ChainIkSolverPos_LMA ik_solver);
-    int forward_kinematics(KDL::JntArray current_joint_position);
-    void joint_state_callback(const sensor_msgs::JointState::ConstPtr &q, KDL::ChainFkSolverPos_recursive fk_solver);
+    KDL::Jacobian get_jacobian();
+    KDL::JntArray inverse_kinematics_closed(KDL::Frame desired_pose);
+    int forward_kinematics(KDL::JntArray current_joint_position, KDL::Frame current_pose);
+    void joint_state_callback(const sensor_msgs::JointState::ConstPtr &q);
 
 };
 
